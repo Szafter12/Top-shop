57e9e067c7b45fd5b6af8d583e8772ea97701a45
JakubPachut <jakubpachut@gmail.com>
2024-11-05 00:28:30 +0100

    finish productlist view

--- front-end/src/views/ProductList/ProductList.jsx
+++ front-end/src/views/ProductList/ProductList.jsx
@@ -4,6 +4,7 @@ import { ExpandableMenu } from '../../components/ExpandableMenu/ExpandableMenu'
 import { Breadcrumbs } from '../../components/Breadcrumbs/Breadcrumbs'
 import { CenterContent } from '../../components/CenterContent/CenterContent'
 import { Products } from '../../components/Products/Products'
+import { Pagination } from '../../components/Pagination/Pagination'
 
 export function ProductList() {
 	return (
@@ -11,11 +12,12 @@ export function ProductList() {
 			<CenterContent>
 				<FlexContainer>
 					<ExpandableMenu />
-					<div style={{width: "100%"}}>
+					<div style={{ width: '100%' }}>
 						<Breadcrumbs />
 						<Products heading='Swetry' />
 					</div>
 				</FlexContainer>
+					<Pagination numbersOfPages={4}/>
 			</CenterContent>
 		</Layout>
 	)
