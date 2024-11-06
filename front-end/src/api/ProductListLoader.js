8b4027ea4b7e434e8614adf9dfb16edfe2e62fe8
Jakub <jakubpachut@gmail.com>
2024-11-06 15:17:59 +0100

    change api

--- front-end/src/api/ProductListLoader.js
+++ front-end/src/api/ProductListLoader.js
@@ -2,13 +2,12 @@ import { redirect } from 'react-router-dom'
 import { PATH_TO_ENDPOINT_MAPPING, BACKEND_URL } from '../constants/api'
 import { CATEGORIES } from '../constants/category'
 
-
 export const ProductListLoader = ({ params: { gender, category } }) => {
 	const foundCategory = CATEGORIES.find(c => c.path === category)
 	const foundGender = PATH_TO_ENDPOINT_MAPPING[gender]
-	console.log(foundCategory);
+	console.log(foundCategory)
 	if (foundCategory && foundGender) {
-		return fetch(`${BACKEND_URL}/products?gender=${foundGender}&category=${foundCategory.path}`)
+		return fetch(`${BACKEND_URL}/products/?gender=${foundGender}&category=${foundCategory.path}`)
 	} else {
 		return redirect('/kobieta')
 	}
