98bafbe35edfa513c0a3b1b60622186bfc88b4e6
Jakub <jakubpachut@gmail.com>
2024-11-06 23:10:25 +0100

    adding new backend

--- front-end/src/constants/api.js
+++ front-end/src/constants/api.js
@@ -1,7 +1,7 @@
 export const PATH_TO_ENDPOINT_MAPPING = {
-    kobieta: 'women',
-    mezczyzna: 'men',
-    dziecko: 'children',
+	kobieta: 'women',
+	mezczyzna: 'men',
+	dziecko: 'children',
 }
 
-export const BACKEND_URL = 'http://localhost:3000'
+export const BACKEND_URL = 'https://top-shop-nap3.onrender.com/'
