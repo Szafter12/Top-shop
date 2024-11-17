5a252424d3adaa0a15ea24fe448da51c4fb1bee4
Jakub <jakubpachut@gmail.com>
2024-11-18 00:42:03 +0100

    fix cart

--- front-end/src/views/Cart/Cart.jsx
+++ front-end/src/views/Cart/Cart.jsx
@@ -1,9 +1,15 @@
 import { useContext } from 'react'
 import { CartList } from '../../components/CartList/CartList'
 import { CartContext } from '../../contexts/CartContext'
+import { HeadContent } from '../../components/HeadContent/HeadContent'
 
 export function Cart() {
 	const [cartItems] = useContext(CartContext)
 
-	return <CartList cartProducts={cartItems} />
+	return (
+		<>
+			<HeadContent description='Zobacz produkty w koszyku'>Koszyk</HeadContent>
+			<CartList cartProducts={cartItems} />
+		</>
+	)
 }
