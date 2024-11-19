4560869563170e8bc9d5bcd1cdee73d2a1bc7833
Jakub <jakubpachut@gmail.com>
2024-11-19 01:12:36 +0100

    adding storybook

---
+++ front-end/src/stories/FullWidthButton.stories.js
@@ -0,0 +1,13 @@
+import { FullWidthBtn } from '../components/FullWidthBtn/FullWidthBtn'
+
+export default {
+	component: FullWidthBtn,
+	title: 'FullWidthBtn',
+}
+
+export const Default = {
+	args: {
+		children: 'testowy przycisk',
+		bgc: 'black',
+	},
+}
